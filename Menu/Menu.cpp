de2776d635d78bfec989d6b121310cd1b31a280d
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-07-03 19:42:10 +0300

    Добавьте файлы проекта.

---
+++ Menu/Menu.cpp
@@ -0,0 +1,17 @@
+#include "ShowMenu.h"
+#include "DirMenu.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main()
+{
+    ColorMenu();
+    Menu();
+    return 0;
+}
